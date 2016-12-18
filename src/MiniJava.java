bd48a96d9d6fb8c49677f195b7ec134704fdd17f
yue yang <this.isssssyy@gmail.com>
2016-12-18 18:11:32 +0800

    [antlr4.g4] add MiniJava.g4, success generate files.

--- src/MiniJava.java
+++ src/MiniJava.java
@@ -1,6 +1,9 @@
+import org.antlr.runtime.ANTLRInputStream;
+
 public class MiniJava {
 
     public static void main(String[] args) throws Exception {
-
+        ANTLRInputStream inputStream = new ANTLRInputStream(System.in);
+        
     }
 }
